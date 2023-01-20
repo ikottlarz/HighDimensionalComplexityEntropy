b55f69d55a976c58121b363834a366ed017028a3
Inga Kottlarz <inga.kottlarz@ds.mpg.de>
2023-01-20 11:28:46 +0100

    update surrogate calc

--- scripts/calc_complexity_entropy.jl
+++ scripts/calc_complexity_entropy.jl
@@ -4,14 +4,14 @@ include(srcdir("complexity_entropy.jl"))
 using ProgressMeter
 using TimeseriesSurrogates
 
-function complexity_entropy(
+function complexity_entropy!(
     data::Dict{String, Any};
     ms::AbstractVector{Int},
     τs::AbstractVector{Int},
     lengths::AbstractVector{Int},
-    dims::AbstractVector{Int}
+    dims::AbstractVector{Int},
+    ce_values::Dict{String, Any}
     )
-    ce_values = Dict{String, Any}()
     @showprogress for dim in dims
         ce_values["dim=$dim"] = Dict{String, Any}()
         for data_length in lengths
@@ -27,7 +27,6 @@ function complexity_entropy(
             end
         end
     end
-    return ce_values
 end
 
 function complexity_entropy(
@@ -39,7 +38,8 @@ function complexity_entropy(
     )
     loaded_file = wload(filename)
     data = loaded_file["data"]
-    ce_values = complexity_entropy(data; ms, τs, lengths, dims)
+    ce_values = Dict{String, Any}()
+    complexity_entropy!(data; ms, τs, lengths, dims, ce_values)
     return Dict("data"=>ce_values, "simulation_parameters"=>loaded_file["parameters"], "parameters"=>@strdict(ms, τs, lengths, dims))
 end
 
@@ -60,24 +60,62 @@ data, filename = produce_or_load(
     filename="mackey_glass"
 )
 
-using CairoMakie
 
-fig = figure()
-ax = fig[1,1]
-m = 6
-data_length = 10^6
-dim = 40
-for τ in 1:50
-    plot!(ax, data["dim=$dim"]["data_length=$data_length"]["m=$m"]["τ$τ"]...)
-end
-
-function surrogate_complexity_entropy(filename::String; num_surrogates, kwargs...)
+function surrogate_complexity_entropy(
+    filename::String,
+    num_surrogates::Int,
+    ms::AbstractVector{Int},
+    τs::AbstractVector{Int},
+    lengths::AbstractVector{Int},
+    dims::AbstractVector{Int}
+    )
     loaded_file = wload(filename)
     x = loaded_file["data"]
     surrogate_ce = Dict{String, Any}()
     for n in 1:num_surrogates
-        sur = surrogate(x, RandomFourier(true))
-        surrogate_ce["n$n"] = complexity_entropy(sur, kwargs...)
+        surrogate_ce["n$n"] = Dict{Str, Any}()
+        for dim in dims
+            for data_length in lengths
+                sur = surrogate(x["τ$dim"][1:data_length], RandomFourier(true))
+                data = Dict{String, Any}("τ$dim"=>sur)
+                complexity_entropy!(
+                    data;
+                    ms, τs, lengths=[data_length], dims=[dim], ce_values=surrogate_ce["n$n"]
+                )
+            end
+        end
     end
     return Dict("data"=>surrogate_ce, "simulation_parameters"=>loaded_file["parameters"], "parameters"=>@strdict(kwargs...))
 end
+
+function surrogate_complexity_entropy(config::NamedTuple)
+    return surrogate_complexity_entropy(config...)
+end
+
+
+surrogate_data, filename = produce_or_load(
+    surrogate_complexity_entropy,
+    (
+        filename=datadir("sims/mackey_glass.jld2"),
+        num_surrogates=50,
+        ms=[3, 4, 5, 6, 7],
+        τs=collect(1:50),
+        lengths=10 .^(3:6),
+        dims=collect(1:50)
+    ),
+    datadir("analysis");
+    filename="mackey_glass_surrogates"
+)
+
+using CairoMakie
+
+fig = Figure()
+ax = Axis(fig[1, 1])
+m = 6
+data_length = 10^6
+dim = 40
+for τ in 1:50
+    entropy, complexity = data["data"]["dim=$dim"]["data_length=$data_length"]["m=$m"]["τ$τ"]
+    plot!(ax, [entropy], [complexity])
+end
+fig
