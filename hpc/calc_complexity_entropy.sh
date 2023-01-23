10ac703b9fdffc3067473e3c1e3bafd3f6498153
Inga Kottlarz <inga.kottlarz@ds.mpg.de>
2023-01-23 10:52:29 +0100

    update paths

--- hpc/calc_complexity_entropy.sh
+++ hpc/calc_complexity_entropy.sh
@@ -2,15 +2,15 @@
 
 fn="`date +"%Y-%m-%d_%H-%M"`_calc_complexity_entropy"
 
-echo "#!/bin/bash" > "$fn.sh"
-echo "#SBATCH -p medium" >> "$fn.sh"
-echo "#SBATCH -c 40" >> "$fn.sh"
-echo "#SBATCH -t 500" >> "$fn.sh"
-echo "#SBATCH --mem-per-cpu 1G" >> "$fn.sh"
-echo "#SBATCH -o hpc/$fn.out" >> "$fn.sh"
+echo "#!/bin/bash" > "slurm_scripts/$fn.sh"
+echo "#SBATCH -p medium" >> "slurm_scripts/$fn.sh"
+echo "#SBATCH -c 40" >> "slurm_scripts/$fn.sh"
+echo "#SBATCH -t 500" >> "slurm_scripts/$fn.sh"
+echo "#SBATCH --mem-per-cpu 1G" >> "slurm_scripts/$fn.sh"
+echo "#SBATCH -o $fn.out" >> "output/$fn.out"
 
-echo "julia -e 'import Pkg; Pkg.add(\"DrWatson\"); using DrWatson; @quickactivate; Pkg.instantiate()'" >> "$fn.sh"
-echo "julia scripts/simulate.jl" >> "$fn.sh"
-echo "julia - t 40 scripts/calc_complexity_entropy.jl" >> "$fn.sh"
+echo "julia -e 'import Pkg; Pkg.add(\"DrWatson\"); using DrWatson; @quickactivate; Pkg.instantiate()'" >> "slurm_scripts/$fn.sh"
+echo "julia scripts/simulate.jl" >> "slurm_scripts/$fn.sh"
+echo "julia - t 40 scripts/calc_complexity_entropy.jl" >> "slurm_scripts/$fn.sh"
 
-sbatch -C scratch $fn.sh
+sbatch -C scratch slurm_scripts/$fn.sh
