f9974380e71592893d1fb08108f367dc5f8bc104
Inga Kottlarz <inga.kottlarz@ds.mpg.de>
2023-01-23 10:42:21 +0100

    change name of output file for slurm

--- hpc/calc_complexity_entropy.sh
+++ hpc/calc_complexity_entropy.sh
@@ -2,6 +2,7 @@
 #SBATCH -c 40
 #SBATCH -t 500
 #SBATCH --mem-per-cpu 1G
+#SBATCH -o hpc/`date +"%Y-%m-%d_%H-%M"`_calc_complexity_entropy.out
 
 julia -e 'import Pkg; Pkg.add("DrWatson"); using DrWatson; @quickactivate; Pkg.instantiate()'
 julia scripts/simulate.jl
