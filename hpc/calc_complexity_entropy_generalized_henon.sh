498e9f3027ca072a3b7fbe815bc89e3e754d0558
Inga Kottlarz <inga.kottlarz@ds.mpg.de>
2023-01-24 17:55:38 +0100

    add absolute paths to hpc scripts

--- hpc/calc_complexity_entropy_generalized_henon.sh
+++ hpc/calc_complexity_entropy_generalized_henon.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 fn="`date +"%Y-%m-%d_%H-%M"`_calc_complexity_entropy_generalized_henon"
+gitdir=`git rev-parse --show-toplevel`
 
-echo "#!/bin/bash" > "slurm_scripts/$fn.sh"
-echo "#SBATCH -p medium" >> "slurm_scripts/$fn.sh"
-echo "#SBATCH -c 50" >> "slurm_scripts/$fn.sh"
-echo "#SBATCH -t 200" >> "slurm_scripts/$fn.sh"
-echo "#SBATCH --mem-per-cpu 2G" >> "slurm_scripts/$fn.sh"
-echo "#SBATCH -o $fn.out" >> "output/$fn.out"
+echo "#!/bin/bash" > "$gitdir/hpc/slurm_scripts/$fn.sh"
+echo "#SBATCH -p medium" >> "$gitdir/hpc/slurm_scripts/$fn.sh"
+echo "#SBATCH -c 50" >> "$gitdir/hpc/slurm_scripts/$fn.sh"
+echo "#SBATCH -t 200" >> "$gitdir/hpc/slurm_scripts/$fn.sh"
+echo "#SBATCH --mem-per-cpu 2G" >> "$gitdir/hpc/slurm_scripts/$fn.sh"
+echo "#SBATCH -o $gitdir/hpc/outputs/$fn.out" >> "$gitdir/hpc/slurm_scripts/$fn.out"
 
-echo "julia -e 'import Pkg; Pkg.add(\"DrWatson\"); using DrWatson; @quickactivate; Pkg.instantiate()'" >> "slurm_scripts/$fn.sh"
-echo "julia - t 50 scripts/calc_complexity_entropy_generalized_henon.jl" >> "slurm_scripts/$fn.sh"
+echo "julia -e 'import Pkg; Pkg.add(\"DrWatson\"); using DrWatson; @quickactivate; Pkg.instantiate()'" >> "$gitdir/hpc/slurm_scripts/$fn.sh"
+echo "julia - t 50 scripts/calc_complexity_entropy_generalized_henon.jl" >> "$gitdir/hpc/slurm_scripts/$fn.sh"
 
-sbatch -C scratch slurm_scripts/$fn.sh
+sbatch -C scratch $gitdir/hpc/slurm_scripts/$fn.sh
--- hpc/calc_complexity_entropy_mackey_glass.sh
+++ hpc/calc_complexity_entropy_mackey_glass.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 fn="`date +"%Y-%m-%d_%H-%M"`_calc_complexity_entropy_mackey_glass"
+gitdir=`git rev-parse --show-toplevel`
 
-echo "#!/bin/bash" > "slurm_scripts/$fn.sh"
-echo "#SBATCH -p medium" >> "slurm_scripts/$fn.sh"
-echo "#SBATCH -c 50" >> "slurm_scripts/$fn.sh"
-echo "#SBATCH -t 200" >> "slurm_scripts/$fn.sh"
-echo "#SBATCH --mem-per-cpu 1G" >> "slurm_scripts/$fn.sh"
-echo "#SBATCH -o $fn.out" >> "output/$fn.out"
+echo "#!/bin/bash" > "$gitdir/hpc/slurm_scripts/$fn.sh"
+echo "#SBATCH -p medium" >> "$gitdir/hpc/slurm_scripts/$fn.sh"
+echo "#SBATCH -c 50" >> "$gitdir/hpc/slurm_scripts/$fn.sh"
+echo "#SBATCH -t 200" >> "$gitdir/hpc/slurm_scripts/$fn.sh"
+echo "#SBATCH --mem-per-cpu 1G" >> "$gitdir/hpc/slurm_scripts/$fn.sh"
+echo "#SBATCH -o $gitdir/hpc/outputs/$fn.out" >> "$gitdir/hpc/slurm_scripts/$fn.out"
 
-echo "julia -e 'import Pkg; Pkg.add(\"DrWatson\"); using DrWatson; @quickactivate; Pkg.instantiate()'" >> "slurm_scripts/$fn.sh"
-echo "julia - t 50 scripts/calc_complexity_entropy_mackey_glass.jl" >> "slurm_scripts/$fn.sh"
+echo "julia -e 'import Pkg; Pkg.add(\"DrWatson\"); using DrWatson; @quickactivate; Pkg.instantiate()'" >> "$gitdir/hpc/slurm_scripts/$fn.sh"
+echo "julia - t 50 scripts/calc_complexity_entropy_mackey_glass.jl" >> "$gitdir/hpc/slurm_scripts/$fn.sh"
 
-sbatch -C scratch $DIRECTORY/hpc/slurm_scripts/$fn.sh
+sbatch -C scratch $gitdir/hpc/slurm_scripts/$fn.sh
