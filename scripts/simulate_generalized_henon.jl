fc5c1fcff7022cb74864de6db7647d87ff098f96
Inga Kottlarz <inga.kottlarz@stud.uni-goettingen.de>
2023-01-24 17:41:46 +0100

    update to DrWatson workflow

--- scripts/simulate_generalized_henon.jl
+++ scripts/simulate_generalized_henon.jl
@@ -4,33 +4,14 @@ using DynamicalSystems
 using ProgressMeter
 include(srcdir("henon.jl"))
 
-function simulate_henon(
-    a::Float64,
-    b::Float64,
-    Dmax::Int,
-    N::Int,
-    Ttr::Int,
-    )
+function simulate_generalized_henon(config::NamedTuple)
+    @unpack a, b, Dmax, N, Ttr = config
     trajectories = Dict{String, Any}()
-    @showprogress for D in 1:Dmax
+    @showprogress for D in 2:Dmax
         u0 = zeros(D)
         ds = DiscreteDynamicalSystem(henons!, u0, [a, b], henons_jac!)
         X = trajectory(ds, N; Ttr = 1000)
-
         trajectories["D$D"] = hcat(X[:, 1]...)
     end
     return Dict("data"=>trajectories, "parameters"=>@strdict(a, b, N, Ttr))
 end
-
-function simulate_henon(config::NamedTuple)
-    return simulate_henon(config...)
-end
-
-config = (
-    a = 1.76,
-    b = 0.1,
-    Dmax = 50,
-    N = 1000000,
-    T = 1000
-)
-produce_or_load(simulate_henon, config, datadir("sims"); filename="generalized_henon")
--- scripts/simulate_mackey_glass.jl
+++ scripts/simulate_mackey_glass.jl
@@ -5,16 +5,8 @@ using ProgressMeter
 
 include(srcdir("mackey_glass.jl"))
 
-function simulate_mackey_glass(;
-    β::Float64,
-    γ::Float64,
-    n::Float64,
-    max_τ::Int64,
-    Δt::Float64,
-    t_sample::Float64,
-    N::Int64,
-    Ttr::Int64
-    )
+function simulate_mackey_glass(config::NamedTuple)
+    @unpack β, γ, n, max_τ, Δt, t_sample, N, Ttr, commit_hash = config
     trajectories = Dict{String, Any}()
     @showprogress for τ in 1:max_τ
         u0 = zeros(Int(τ/Δt))
@@ -27,24 +19,7 @@ function simulate_mackey_glass(;
 
         X = X[1:steps_per_sample:end, 1]
 
-        trajectories["τ$τ"] = X
+        trajectories["dim$τ"] = X
     end
     return Dict("data"=>trajectories, "parameters"=>@strdict(β, γ, n, Δt, N, Ttr, t_sample))
 end
-
-
-function simulate_mackey_glass(config::NamedTuple)
-    simulate_mackey_glass(;config...)
-end
-
-config = (
-    β = 2.,
-    γ = 1.,
-    n = 9.65,
-    max_τ = 50,
-    Δt = 0.1,
-    t_sample = 0.2,
-    N = 1000000,
-    Ttr = 1000,
-)
-produce_or_load(simulate_mackey_glass, config, datadir("sims"); filename="mackey_glass")
