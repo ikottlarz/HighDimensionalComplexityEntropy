afa9a6876ee6033a0305648dc8e449c0d5357acf
Inga Kottlarz <inga.kottlarz@ds.mpg.de>
2023-01-24 13:44:35 +0100

    add absolute paths to bashscript

--- hpc/calc_complexity_entropy.sh
+++ hpc/calc_complexity_entropy.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 fn="`date +"%Y-%m-%d_%H-%M"`_calc_complexity_entropy"
+DIRECTORY=`git rev-parse --show-toplevel`
 
-echo "#!/bin/bash" > "slurm_scripts/$fn.sh"
-echo "#SBATCH -p medium" >> "slurm_scripts/$fn.sh"
-echo "#SBATCH -c 40" >> "slurm_scripts/$fn.sh"
-echo "#SBATCH -t 500" >> "slurm_scripts/$fn.sh"
-echo "#SBATCH --mem-per-cpu 1G" >> "slurm_scripts/$fn.sh"
-echo "#SBATCH -o $fn.out" >> "output/$fn.out"
+echo "#!/bin/bash" > "$DIRECTORY/hpc/slurm_scripts/$fn.sh"
+echo "#SBATCH -p medium" >> "$DIRECTORY/hpc/slurm_scripts/$fn.sh"
+echo "#SBATCH -c 40" >> "$DIRECTORY/hpc/slurm_scripts/$fn.sh"
+echo "#SBATCH -t 500" >> "$DIRECTORY/hpc/slurm_scripts/$fn.sh"
+echo "#SBATCH --mem-per-cpu 1G" >> "$DIRECTORY/hpc/slurm_scripts/$fn.sh"
+echo "#SBATCH -o $DIRECTORY/hpc/outputs/$fn.out" >> "$DIRECTORY/hpc/slurm_scripts/$fn.sh"
 
-echo "julia -e 'import Pkg; Pkg.add(\"DrWatson\"); using DrWatson; @quickactivate; Pkg.instantiate()'" >> "slurm_scripts/$fn.sh"
-echo "julia scripts/simulate.jl" >> "slurm_scripts/$fn.sh"
-echo "julia - t 40 scripts/calc_complexity_entropy.jl" >> "slurm_scripts/$fn.sh"
+echo "julia -e 'import Pkg; Pkg.add(\"DrWatson\"); using DrWatson; @quickactivate; Pkg.instantiate()'" >> "$DIRECTORY/hpc/slurm_scripts/$fn.sh"
+echo "julia ../scripts/simulate.jl" >> "$DIRECTORY/hpc/slurm_scripts/$fn.sh"
+echo "julia - t 40 ../scripts/calc_complexity_entropy.jl" >> "$DIRECTORY/hpc/slurm_scripts/$fn.sh"
 
-sbatch -C scratch slurm_scripts/$fn.sh
+sbatch -C scratch $DIRECTORY/hpc/slurm_scripts/$fn.sh
