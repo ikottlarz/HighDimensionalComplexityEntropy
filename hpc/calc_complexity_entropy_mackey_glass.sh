b819bf6a3917fc7afcb5078a885a84379132156c
Inga Kottlarz <inga.kottlarz@ds.mpg.de>
2023-01-24 18:24:48 +0100

    update hardware dependencies

--- hpc/calc_complexity_entropy_mackey_glass.sh
+++ hpc/calc_complexity_entropy_mackey_glass.sh
@@ -7,7 +7,7 @@ echo "#!/bin/bash" > "$gitdir/hpc/slurm_scripts/$fn.sh"
 echo "#SBATCH -p medium" >> "$gitdir/hpc/slurm_scripts/$fn.sh"
 echo "#SBATCH -c 50" >> "$gitdir/hpc/slurm_scripts/$fn.sh"
 echo "#SBATCH -t 200" >> "$gitdir/hpc/slurm_scripts/$fn.sh"
-echo "#SBATCH --mem-per-cpu 1G" >> "$gitdir/hpc/slurm_scripts/$fn.sh"
+echo "#SBATCH --mem-per-cpu 2G" >> "$gitdir/hpc/slurm_scripts/$fn.sh"
 echo "#SBATCH -o $gitdir/hpc/outputs/$fn.out" >> "$gitdir/hpc/slurm_scripts/$fn.sh"
 
 echo "julia -e 'import Pkg; Pkg.add(\"DrWatson\"); using DrWatson; @quickactivate; Pkg.instantiate()'" >> "$gitdir/hpc/slurm_scripts/$fn.sh"
