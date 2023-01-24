0aba306e5c4f872f5260e9742543de273000a58c
Inga Kottlarz <inga.kottlarz@stud.uni-goettingen.de>
2023-01-24 17:42:13 +0100

    restructure CE calculations

--- scripts/calc_complexity_entropy.jl
+++ scripts/calc_complexity_entropy.jl
@@ -1,95 +1,80 @@
 using DrWatson
 @quickactivate
 include(srcdir("complexity_entropy.jl"))
-using ProgressMeter
-using TimeseriesSurrogates
 include(srcdir("threadsafe_dict.jl"))
 using .ThreadsafeDict
 
 """
-    function complexity_entropy!(data; ms, τs, lengths, dims, ce_values::Dict)
+function complexity_entropy!(time_series; ms, τs, ce_values::Dict)
 
-This function calculates the statistical complexity and permutation entropy for
-all
+This function calculates the statistical complexity and permutation entropy
+of `time_series` for all combinations of word lengths (`ms`), lags (`τs`).
+
+Fills a Dict `ce_values` with entries
+    - "m=\$m"
+        - "τ=\$τ"
 """
 function complexity_entropy!(
-    data::Dict{String, Any};
+    time_series::AbstractVector{Float64};
     ms::AbstractVector{Int},
     τs::AbstractVector{Int},
-    lengths::AbstractVector{Int},
-    dims::AbstractVector{Int},
     ce_values::Dict{String, Dict}
     )
+    for m in ms
+        d = dictsrv(Dict{String, Vector{Float64}}())
+        Threads.@threads for τ in τs
+            est = SymbolicPermutation(; m, τ)
+            entropy, complexity = entropy_stat_complexity(est, time_series)
+            d["τ$τ"]  = [entropy,  complexity]
+        end
+        ce_values["m=$m"] = d()
+    end
+end
+
+function complexity_entropy(config::NamedTuple)
+    @unpack filename_prefix, τs, ms, dims, lengths, simulation_parameters, data_producing_function = config
+    data, _ = produce_or_load(
+        data_producing_function,
+        simulation_parameters,
+        datadir("sims");
+        filename=hash,
+        prefix=filename_prefix
+    )
+    ce_values = Dict{String, Dict}()
     @showprogress for dim in dims
         ce_values["dim=$dim"] = Dict{String, Dict}()
         for data_length in lengths
             ce_values["dim=$dim"]["data_length=$data_length"] = Dict{String, Dict}()
-            ts = data["τ$dim"][1:data_length]
-            for m in ms
-                d = dictsrv(Dict{String, Vector{Float64}}())
-                Threads.@threads for τ in τs
-                    est = SymbolicPermutation(; m, τ)
-                    entropy, complexity = entropy_stat_complexity(est, ts)
-                    d["τ$τ"]  = [entropy,  complexity]
-                end
-                ce_values["dim=$dim"]["data_length=$data_length"]["m=$m"] = d()
-            end
+            complexity_entropy!(
+                data["data"][1:data_length];
+                ms, τs,
+                ce_values=ce_values["dim=$dim"]["data_length=$data_length"]
+            )
         end
     end
+    return Dict("data"=>ce_values, "simulation_parameters"=>simulation_parameters, "parameters"=>@strdict(ms, τs, lengths, dims))
 end
 
-function complexity_entropy(
-    filename::String,
-    ms::AbstractVector{Int},
-    τs::AbstractVector{Int},
-    lengths::AbstractVector{Int},
-    dims::AbstractVector{Int}
+function surrogate_complexity_entropy(config::NamedTuple)
+    @unpack filename_prefix, τs, ms, dims, lengths, num_surrogates, simulation_parameters, data_producing_function = config
+    data, _ = produce_or_load(
+        data_producing_function,
+        simulation_parameters,
+        datadir("sims");
+        filename=hash,
+        prefix=filename_prefix
     )
-    loaded_file = wload(filename)
-    data = loaded_file["data"]
-    ce_values = Dict{String, Dict}()
-    complexity_entropy!(data; ms, τs, lengths, dims, ce_values)
-    return Dict("data"=>ce_values, "simulation_parameters"=>loaded_file["parameters"], "parameters"=>@strdict(ms, τs, lengths, dims))
-end
-
-function complexity_entropy(config)
-    return complexity_entropy(config...)
-end
-
-data, filename = produce_or_load(
-    complexity_entropy,
-    (
-        filename=datadir("sims/mackey_glass.jld2"),
-        ms=[3, 4, 5, 6, 7],
-        τs=collect(1:50),
-        lengths=10 .^(3:6),
-        dims=collect(1:50)
-    ),
-    datadir("analysis");
-    filename="mackey_glass"
-)
-
-
-function surrogate_complexity_entropy(
-    filename::String,
-    num_surrogates::Int,
-    ms::AbstractVector{Int},
-    τs::AbstractVector{Int},
-    lengths::AbstractVector{Int},
-    dims::AbstractVector{Int}
-    )
-    loaded_file = wload(filename)
-    x = loaded_file["data"]
     surrogate_ce = Dict{String, Dict}()
     for n in 1:num_surrogates
-        surrogate_ce["n$n"] = Dict{String, Dict}()
+        surrogate_ce["n=$n"] = Dict{String, Dict}()
         @showprogress for dim in dims
+            surrogate_ce["n=$n"]["dim=$dim"] = Dict{String, Dict}()
             for data_length in lengths
-                sur = surrogate(x["τ$dim"][1:data_length], RandomFourier(true))
-                data = Dict{String, Any}("τ$dim"=>sur)
+                surrogate_ce["n=$n"]["dim=$dim"]["data_length=$data_length"] = Dict{String, Dict}()
+                sur = surrogate(data["dim$dim"][1:data_length], RandomFourier(true))
                 complexity_entropy!(
-                    data;
-                    ms, τs, lengths=[data_length], dims=[dim], ce_values=surrogate_ce["n$n"]
+                    sur;
+                    ms, τs, ce_values=surrogate_ce["n$n"]["dim=$dim"]["data_length=$data_length"]
                 )
             end
         end
@@ -103,35 +88,3 @@ function surrogate_complexity_entropy(
         )
     return Dict("data"=>surrogate_ce, "simulation_parameters"=>loaded_file["parameters"], "parameters"=>parameters)
 end
-
-function surrogate_complexity_entropy(config::NamedTuple)
-    return surrogate_complexity_entropy(config...)
-end
-
-
-surrogate_data, filename = produce_or_load(
-    surrogate_complexity_entropy,
-    (
-        filename=datadir("sims/mackey_glass.jld2"),
-        num_surrogates=50,
-        ms=[3, 4, 5, 6, 7],
-        τs=collect(1:50),
-        lengths=10 .^(3:6),
-        dims=collect(1:50)
-    ),
-    datadir("analysis");
-    filename="mackey_glass_surrogates"
-)
-
-using CairoMakie
-
-fig = Figure()
-ax = Axis(fig[1, 1])
-m = 6
-data_length = 10^6
-dim = 40
-for τ in 1:50
-    entropy, complexity = data["data"]["dim=$dim"]["data_length=$data_length"]["m=$m"]["τ$τ"]
-    plot!(ax, [entropy], [complexity])
-end
-fig
