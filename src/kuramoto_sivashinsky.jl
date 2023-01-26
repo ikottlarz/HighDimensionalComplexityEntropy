5cbb404a3c8d8bc7ced435ff77e11fe337be3225
Inga Kottlarz <inga.kottlarz@stud.uni-goettingen.de>
2023-01-26 15:15:16 +0100

    initial commit of function

---
+++ src/kuramoto_sivashinsky.jl
@@ -0,0 +1,14 @@
+using LinearAlgebra: mul!
+import FFTW
+
+function kse_spectral!(dy, y, p, t)
+    forward_plan, inverse_plan, udummy, ydummy, k²_k⁴, ik2 = p
+    y² = begin # nonlinear term
+        mul!(udummy, inverse_plan, y) # create current u in x-space
+        udummy .*= udummy # square current u
+        mul!(ydummy, forward_plan, udummy) # transform to k-space
+    end
+    # KS equation in spectral space
+    @. dy = y²*ik2 + k²_k⁴*y
+    return nothing
+end
