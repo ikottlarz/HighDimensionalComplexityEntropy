9f7e00deaa1bd96b75fb49946e85311eda292d6c
Inga Kottlarz <inga.kottlarz@ds.mpg.de>
2023-02-05 11:21:28 +0000

    add b&w cbars for fixed quantities logscale for data length not yet working as desired.

--- scripts/standard_plot_generation.jl
+++ scripts/standard_plot_generation.jl
@@ -5,13 +5,22 @@ using CairoMakie
 
 include(projectdir("configs/base.jl"))
 
-function standard_figure(; cbar_label::String, cbar_limits::Tuple)
+function standard_figure(;
+    cbar_label::String,
+    cbar_limits::Tuple,
+    quantity::Symbol,
+    fix_quantities_for_plot)
     set_theme!(
         Theme(
             colormap=:hawaii,
             markersize=20
         )
     )
+    # copy info dict and delete fix value of quantity that will
+    # be iterated in this plot
+    info_dict = copy(fixed_quantities["values"])
+    delete!(info_dict, quantity)
+
     fig = Figure(resolution=(800, 900))
     ga = fig[1:8, 2:3] = GridLayout()
     ca = fig[10, 2] = GridLayout()
@@ -24,9 +33,14 @@ function standard_figure(; cbar_label::String, cbar_limits::Tuple)
     kuramoto_sivashinsky = Axis(ga[2, 2], title="Kuramoto-Sivashinsky")
     linkaxes!(lorenz_96, generalized_henon, mackey_glass, kuramoto_sivashinsky)
     Colorbar(
-        ca[1, 1], colormap=:hawaii, limits=cbar_limits,
+        ca[1, 1],
+        colormap=cgrad(
+            :hawaii,
+            scale=fix_quantities_for_plot[quantity]["scale"]),
+        limits=cbar_limits,
         vertical=false, label = cbar_label,
-        flipaxis=false
+        flipaxis=false,
+        scale=fix_quantities_for_plot[quantity]["scale"]
     )
 
     Label(fig[1:8, 1], ylabel, rotation=pi/2)
@@ -45,6 +59,36 @@ function standard_figure(; cbar_label::String, cbar_limits::Tuple)
         ["original", "surrogates"],
         framevisible=false
     )
+
+    meta_layout = fig[1:10, 4] = GridLayout()
+    for (i, (q, value)) in enumerate(info_dict)
+        Colorbar(
+            meta_layout[i, 1],
+            limits=fix_quantities_for_plot[q]["limits"],
+            label=fix_quantities_for_plot[q]["label"],
+            colormap=cgrad(
+                :grayC,
+                scale=fix_quantities_for_plot[q]["scale"]
+            ),
+            scale=fix_quantities_for_plot[q]["scale"]
+        )
+        cb_ax = Axis(meta_layout[i, 1])
+        ylims!(cb_ax, fix_quantities_for_plot[q]["limits"])
+        hidedecorations!(cb_ax)
+        lines!(
+            cb_ax,
+            [0, 1],
+            ones(2)*value,
+            linewidth=5,
+            color=ones(2)*value,
+            colormap=cgrad(
+                :hawaii,
+                scale = fix_quantities_for_plot[q]["scale"]
+            ),
+            colorrange=fix_quantities_for_plot[q]["limits"],
+        )
+    end
+
     return (
         fig=fig,
         lorenz_96=lorenz_96,
@@ -65,7 +109,8 @@ function plot_system!(
     scatter!(
         ax,
         originals[:, :entropy], originals[:, :complexity],
-        marker=:circle, color=originals[:, single_iterator_names[iterator_quantity_name]],
+        marker=:circle, color=originals[:,
+        single_iterator_names[iterator_quantity_name]],
         strokecolor=:black, strokewidth=0.5,
         colorrange=(minimum(iterator_quantity), maximum(iterator_quantity))
     )
@@ -94,26 +139,41 @@ single_iterator_names = Dict(
     "dims" => :dim
 )
 fixed_quantities = Dict(
-    "τs" => Dict(
+    "functions" => Dict(
         :dim => dim -> dim .== 38,
         :m => m -> m .== 6,
-        :data_length => data_length -> data_length .== 10^6
+        :data_length => data_length -> data_length .== 10^6,
+        :τ => τ -> τ .== 10,
     ),
-    "ms" => Dict(
-        :dim => dim -> dim .== 38,
-        :τ => τ -> τ .== 6,
-        :data_length => data_length -> data_length .== 10^6
+    "values" => Dict(
+        :dim => 38,
+        :m => 6,
+        :data_length => 10^6,
+        :τ => 10
+    )
+)
+
+fix_quantities_for_plot = Dict(
+    :dim => Dict(
+        "limits" => (1, 50),
+        "label" => L"$\Delta^{(KY)}$",
+        "scale" => identity
     ),
-    "lengths" => Dict(
-        :dim => dim -> dim .== 38,
-        :m => m -> m .== 6,
-        :τ => τ -> τ .== 6
+    :m => Dict(
+        "limits" => (3, 7),
+        "label" => "pattern length",
+        "scale" => identity
     ),
-    "dims" => Dict(
-        :data_length => length -> length .== 10^6,
-        :m => m -> m .== 6,
-        :τ => τ -> τ .== 6
+    :data_length => Dict(
+        "limits" => (10^3, 10^6),
+        "label" => "data length",
+        "scale" => log10
     ),
+    :τ => Dict(
+        "limits" => (1, 50),
+        "label" => L"lag [$\delta t$]",
+        "scale" => identity
+    )
 )
 
 cbar_labels = Dict(
@@ -143,17 +203,23 @@ end
 
 for (quantity_name, quantity) in iterator_quantities
     @unpack fig, lorenz_96, generalized_henon, mackey_glass, kuramoto_sivashinsky = standard_figure(
+        ;
         cbar_label=cbar_labels[quantity_name],
-        cbar_limits=(minimum(quantity), maximum(quantity)))
+        cbar_limits=(minimum(quantity), maximum(quantity)),
+        quantity=single_iterator_names[quantity_name],
+        fix_quantities_for_plot
+    )
     for (system_name, system_ax) in @strdict(lorenz_96, generalized_henon, mackey_glass, kuramoto_sivashinsky)
+        fix_qs_copy = copy(fixed_quantities["functions"])
+        delete!(fix_qs_copy, single_iterator_names[quantity_name])
         @unpack originals, surrogates = data[system_name]
         filtered_originals = subset(
             originals,
-            fixed_quantities[quantity_name]...
+            fix_qs_copy...
         )
         filtered_surrogates = subset(
             surrogates,
-            fixed_quantities[quantity_name]...
+            fix_qs_copy...
         )
         plot_system!(
             system_ax,
@@ -163,5 +229,5 @@ for (quantity_name, quantity) in iterator_quantities
             quantity_name
         )
     end
-    save(plotsdir("$quantity_name.eps"), fig)
+    wsave(plotsdir("$quantity_name.eps"), fig)
 end
