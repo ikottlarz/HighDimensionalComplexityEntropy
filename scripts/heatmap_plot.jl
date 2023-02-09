4116b9a858039878d319ca973fa7160eecd1a1a1
Inga Kottlarz <inga.kottlarz@ds.mpg.de>
2023-02-09 19:49:19 +0000

    change fonts

--- scripts/heatmap_plot.jl
+++ scripts/heatmap_plot.jl
@@ -11,7 +11,7 @@ struct SignificanceTicks end
 # we need this custom function to make our colorbar tick labels
 function Makie.get_ticks(::SignificanceTicks, any_scale, ::Makie.Automatic, vmin, vmax)
     vals_h = [0, 1]
-    labels = [val > 0.5 ? "significant" : "not significant" for val in vals_h]
+    labels = [val > 0.5 ? L"significant $ $" : L"not significant $ $" for val in vals_h]
 
     vals_h, labels
 end
@@ -21,16 +21,27 @@ struct MTicks end
 # we need this custom function to make our m tick labels
 function Makie.get_ticks(::MTicks, any_scale, ::Makie.Automatic, vmin, vmax)
     vals_h = collect(1:length(ms))
-    labels = string.(ms)
+    labels = [L"%$(m)" for m in ms]
 
     vals_h, labels
 end
 
-function heatmap_figure(; xticks::Union{AbstractVector, UnitRange})
+struct XTicks end
+
+# we need this custom function to make our m tick labels
+function Makie.get_ticks(::XTicks, any_scale, ::Makie.Automatic, vmin, vmax)
+    vals_h = collect(5:5:length(τs))
+    labels = [L"%$(m)" for m in τs[5:5:end]]
+
+    vals_h, labels
+end
+
+function heatmap_figure()
     set_theme!(
         Theme(
             colormap=:hawaii,
-            markersize=20
+            markersize=20,
+            fontsize=32,
         )
     )
     fig = Figure(resolution=(800, 900))
@@ -40,25 +51,21 @@ function heatmap_figure(; xticks::Union{AbstractVector, UnitRange})
     hen_a = ga[2, 1] = GridLayout()
     mg_a = ga[3, 1] = GridLayout()
     ks_a = ga[4, 1] = GridLayout()
-    xlabel = "lag [\$\\delta t\$]"
-    ylabel = "pattern length"
-    lorenz_96 = Axis(lor_a[1, 1], title="Lorenz-96"; xticks, yticks=MTicks())
-    generalized_henon = Axis(hen_a[1, 1], title="Generalized Henon"; xticks, yticks=MTicks())
-    mackey_glass = Axis(mg_a[1, 1], title="Mackey-Glass"; xticks, yticks=MTicks())
-    kuramoto_sivashinsky = Axis(ks_a[1, 1], title="Kuramoto-Sivashinsky"; xticks, yticks=MTicks(), xlabel)
+    xlabel = L"lag [$\delta t$]"
+    ylabel = L"pattern length $ $"
+    xticks = XTicks()
+    yticks = MTicks()
+    lorenz_96 = Axis(lor_a[1, 1], title=L"Lorenz-96 $ $"; xticks, yticks)
+    generalized_henon = Axis(hen_a[1, 1], title=L"Generalized Henon $ $"; xticks, yticks)
+    mackey_glass = Axis(mg_a[1, 1], title=L"Mackey-Glass $ $"; xticks, yticks)
+    kuramoto_sivashinsky = Axis(ks_a[1, 1], title=L"Kuramoto-Sivashinsky $ $"; xticks, yticks, xlabel)
     linkaxes!(lorenz_96, generalized_henon, mackey_glass, kuramoto_sivashinsky)
 
-    for (label, layout) in zip(["A", "B", "C", "D"], [lor_a, hen_a, mg_a, ks_a])
-        Label(layout[1, 1, TopLeft()], label,
-            font = :bold,
-            padding = (0, 5, 5, 0),
-            halign = :right)
-    end
     Label(fig[1:4, 1], ylabel, rotation=pi/2)
 
     Colorbar(
         ca[1, 1], colormap=cgrad(:hawaii, 2, categorical = true),
-        vertical=true, label = "significance", limits=(-.5, 1.5),
+        vertical=true, label = L"significance $ $", limits=(-.5, 1.5),
         flipaxis=true, ticks=SignificanceTicks(), ticklabelrotation=pi/2
     )
     return (
@@ -74,7 +81,7 @@ end
 
 dim = 38
 data_length = 10^6
-@unpack fig, lorenz_96, generalized_henon, mackey_glass, kuramoto_sivashinsky = heatmap_figure(; xticks = τs[5:5:end])
+@unpack fig, lorenz_96, generalized_henon, mackey_glass, kuramoto_sivashinsky = heatmap_figure()
 for (system_name, system_ax) in @strdict(lorenz_96, generalized_henon, mackey_glass, kuramoto_sivashinsky)
     file, _ = produce_or_load(significance_heatmap, system_configs[system_name], datadir("analysis"); filename=hash, prefix="$(system_name)_significances")
     data = file["data"]
@@ -91,4 +98,4 @@ for (system_name, system_ax) in @strdict(lorenz_96, generalized_henon, mackey_gl
     )
 
 end
-save(plotsdir("significance_heatmap.eps"), fig)
+save(plotsdir("significance_heatmap.pdf"), fig)
