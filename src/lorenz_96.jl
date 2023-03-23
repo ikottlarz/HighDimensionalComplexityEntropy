ed6e76368168d0035bde66df7517ad2868b47c7a
Inga Kottlarz <inga.kottlarz@ds.mpg.de>
2023-03-23 17:07:48 +0100

    adjust lorenz-96 to DynamicalSystems v3.0

---
+++ src/lorenz_96.jl
@@ -0,0 +1,55 @@
+using LinearAlgebra
+
+"""
+    function lorenz96_rule(du, u, p, t)
+
+Implementation of the lorenz96 rule taken from
+DynamicalSystems.jl
+"""
+function lorenz96_rule!(du, u, p, t)
+    F = p[1]; N = length(u)
+    # 3 edge cases
+    du[1] = (u[2] - u[N - 1]) * u[N] - u[1] + F
+    du[2] = (u[3] - u[N]) * u[1] - u[2] + F
+    du[N] = (u[1] - u[N - 2]) * u[N - 1] - u[N] + F
+    # then the general case
+    for n in 3:(N - 1)
+        du[n] = (u[n + 1] - u[n - 2]) * u[n - 1] - u[n] + F
+    end
+    return nothing # always `return nothing` for in-place form!
+end
+
+
+"""
+    function lorenz96_jacob!(J, u, p, t)
+
+Calculate jacobian matrix of N-dimensional Lorenz-96
+system, where N=length(u).
+"""
+function lorenz96_jacob!(J, u, p, t)
+    N = length(u)
+
+    # diagonal
+    J[diagind(J)] .= -1
+
+    # edge cases
+    J[1, 2] = u[N]
+    J[1, N-1] = -u[N]
+    J[1, N] = u[2] - u[N-1]
+
+    J[2, 1] = u[3] - u[N]
+    J[2, 3] = u[1]
+    J[2, N] = -u[1]
+
+    J[N, 1] = u[N-1]
+    J[N, N-2] = -u[N-1]
+    J[N, N-1] = u[1] - u[N - 2]
+
+    # general case
+    for n in 3:(N-1)
+        J[n, n+1] = u[n-1]
+        J[n, n-2] = -u[n-1]
+        J[n, n-1] = u[n + 1] - u[n - 2]
+    end
+    return nothing
+end
