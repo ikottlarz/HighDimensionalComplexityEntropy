92f640701eb20174770869df5e13fe5ccf01b770
Inga Kottlarz <inga.kottlarz@ds.mpg.de>
2023-03-31 13:12:00 +0200

    add hypothesis test for surrogate pe distributions

--- scripts/ft_vs_aaft_surrogates.jl
+++ scripts/ft_vs_aaft_surrogates.jl
@@ -1,20 +1,23 @@
 using DrWatson
 @quickactivate
 include(projectdir("configs/base.jl"))
-using TimeseriesSurrogates, Random
+using TimeseriesSurrogates, Random, HypothesisTests
 using DataFrames, ComplexityMeasures
 using CairoMakie, ProgressMeter
+using Printf
 
 set_theme!(
     Theme(
         markersize=20,
-        fontsize=20,
+        fontsize=25,
         Axis = (
-            titlefont = projectdir("cmu/cmunrm.ttf"),
+            titlefont = projectdir("cmu/cmunbx.ttf"),
             xlabelfont=projectdir("cmu/cmunrm.ttf"),
             ylabelfont=projectdir("cmu/cmunrm.ttf"),
             xticklabelfont=projectdir("cmu/cmunrm.ttf"),
             yticklabelfont=projectdir("cmu/cmunrm.ttf"),
+            yticklabelsize=20,
+            xticklabelsize=20,
         ),
         Legend = (
             labelfont=projectdir("cmu/cmunrm.ttf"),
@@ -91,80 +94,92 @@ function pe_aape(config::NamedTuple)
     return @strdict(data, parameters)
 end
 
-fig = Figure(
-    resolution=(800, 900),
+for pe in [:permutation_entropy, :amplitude_aware_permutation_entropy]
+
+    fig = Figure(
+        resolution=(800, 900),
+        )
+    lorenz_96 = Axis(fig[1:4, 2:5]; title="Lorenz-96", xticks=[0.9962, 0.9964, 0.9966])
+    generalized_henon = Axis(fig[1:4, 6:9]; title="Generalized Hénon", xticks=[0.99993, 0.99994, 0.99995])
+    mackey_glass = Axis(fig[5:8, 2:5]; title="Mackey-Glass")
+    kuramoto_sivashinsky = Axis(fig[5:8, 6:9]; title="Kuramoto-Sivashinsky", xticks=[0.9960, 0.9962, 0.9964])
+    la = fig[10, :] = GridLayout()
+    layouts = @strdict(lorenz_96, generalized_henon, mackey_glass, kuramoto_sivashinsky)
+
+    lorenz_96_legend = fig[1, 2] = GridLayout()
+    henon_legend = fig[1, 6] = GridLayout()
+    mackey_glass_legend = fig[5, 2] = GridLayout()
+    ksiva_legend = fig[5, 6] = GridLayout()
+
+    Label(fig[1:8, 1], "density", rotation=pi/2, font=projectdir("cmu/cmunrm.ttf"))
+    Label(fig[9, 2:end], L"H_S")
+
+    legend_layouts = Dict(
+        "lorenz_96"=>lorenz_96_legend,
+        "mackey_glass"=>mackey_glass_legend,
+        "generalized_henon"=>henon_legend,
+        "kuramoto_sivashinsky"=>ksiva_legend
     )
-lorenz_96 = fig[1:4, 1] = GridLayout()
-generalized_henon = fig[1:4, 2] = GridLayout()
-mackey_glass = fig[5:8, 1] = GridLayout()
-kuramoto_sivashinsky = fig[5:8, 2] = GridLayout()
-la = fig[9, :] = GridLayout()
-layouts = @strdict(lorenz_96, generalized_henon, mackey_glass, kuramoto_sivashinsky)
 
-for (label, system) in zip(["A", "B", "C", "D"], ["lorenz_96", "generalized_henon", "mackey_glass", "kuramoto_sivashinsky"])
-    layout = layouts[system]
-    Label(layout[1, 1, TopLeft()], label,
-        padding = (0, 5, 5, 0),
-        halign = :right)
-end
+    system_names = Dict(
+        "lorenz_96"=>"Lorenz-96",
+        "generalized_henon"=>"Generalized Hénon",
+        "mackey_glass"=>"Mackey-Glass",
+        "kuramoto_sivashinsky"=>"Kuramoto-Sivashinsky"
+    )
 
-for system in ["lorenz_96", "generalized_henon", "mackey_glass", "kuramoto_sivashinsky"]
-    file, _ = produce_or_load(pe_aape, (m=6, τ=10, ky_dim=43, system=system, data_length=10^6, n_sur=400), datadir("analysis"), filename=hash, prefix="$(system)_pe_aape")
-    @show system
-    layout = layouts[system]
-    data = file["data"]
-    pe_ax = Axis(layout[1, :]; title="PE")
-    aape_ax = Axis(layout[2, :]; title="Amplitude Aware PE")
-
-    hist!(
-        pe_ax,
-        subset(
+    for system in ["lorenz_96", "generalized_henon", "mackey_glass", "kuramoto_sivashinsky"]
+        file, _ = produce_or_load(pe_aape, (m=6, τ=10, ky_dim=43, system=system, data_length=10^6, n_sur=400), datadir("analysis"), filename=hash, prefix="$(system)_pe_aape")
+        @show system
+        pe_ax = layouts[system]
+        data = file["data"]
+        # aape_ax = Axis(layout[2, :]; title="Amplitude Aware PE")
+        aaft_pe = subset(
             data,
             :surrogate_algorithm => x-> x.==:AAFT,
-        )[:, :permutation_entropy],
-        color=(:maroon, 0.75)
-    )
-    hist!(
-        pe_ax,
-        subset(
+        )[:, pe]
+        hist!(
+            pe_ax,
+            aaft_pe;
+            color=(:maroon, 0.75),
+            density=true
+        )
+        ft_pe = subset(
             data,
             :surrogate_algorithm => x-> x.==:FT,
-        )[:, :permutation_entropy],
-        color=(:teal, 0.75)
-    )
-    hist!(
-        aape_ax,
-        subset(
-            data,
-            :surrogate_algorithm => x-> x.==:AAFT,
-        )[:, :amplitude_aware_permutation_entropy],
-        color=(:maroon, 0.75)
-    )
-    hist!(
-        aape_ax,
-        subset(
-            data,
-            :surrogate_algorithm => x-> x.==:FT,
-        )[:, :amplitude_aware_permutation_entropy],
-        color=(:teal, 0.75)
+        )[:, pe]
+        hist!(
+            pe_ax,
+            ft_pe;
+            color=(:teal, 0.75),
+            density=true
+        )
+        p = HypothesisTests.pvalue(
+            ApproximateTwoSampleKSTest(
+                ft_pe,
+                aaft_pe
+            )
+        )
+        p_scientific = p < 1e-10 ? L"p\,<\, 10^{-10}" : L"p \approx %$(round(p; digits=2))"
+        Legend(legend_layouts[system][1, 1], [MarkerElement(color=:white, marker=:rect)], [p_scientific]; framevisible=false, labelsize=20)
+
+    end
+
+    Legend(
+        la[1, 1],
+        [LineElement(
+            color=(:maroon, 0.75),
+            linewidth=20
+        ),
+        LineElement(
+            color=(:teal, 0.75),
+            linewidth=20
+        )],
+        ["AAFT surrogates", "FT surrogates"];
+        nbanks=2
     )
 
+
+
+    safesave(plotsdir("surrogate_$(pe)_inspection.pdf"), fig)
 end
-
-Legend(
-    la[1, 1],
-    [LineElement(
-        color=(:maroon, 0.75),
-        linewidth=20
-    ),
-    LineElement(
-        color=(:teal, 0.75),
-        linewidth=20
-    )],
-    ["AAFT surrogates", "FT surrogates"];
-    nbanks=2
-)
-
-
-
-safesave(plotsdir("surrogate_pe_inspection.png"), fig)
